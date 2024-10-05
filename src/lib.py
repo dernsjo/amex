02c1d7fefa59109d8cf1c57baf3e47c7e23634e4
dernsjo <81687304+dernsjo@users.noreply.github.com>
2024-10-05 18:49:27 +0200

    Add paid by features (#4)
    * change in gitignore
    
    * remove ebba_test
    
    * ignore warnings
    
    * upade gitignore
    
    * add load_data
    
    * add calculate_debt

--- src/lib.py
+++ src/lib.py
@@ -5,3 +5,8 @@ def format_data(df: pd.DataFrame) -> pd.DataFrame:
     df['Belopp'] = df['Belopp'].str.replace(',', '.').astype(float)
     df['Datum'] = pd.to_datetime(df['Datum'])    
     return df
+
+def load_data() -> pd.DataFrame:
+    """Function that load data"""
+    df = pd.read_csv('../data/raw/activity.csv')
+    return df
